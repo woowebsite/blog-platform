59f0d338a7c371e3f04c917e1c2169292aac63e8
unknown <wooowebsite@gmail.com>
2022-01-11 21:03:04 +0700

    TextEditable: Fix ref

--- packages/admin/components/ComboBox/index.tsx
+++ packages/admin/components/ComboBox/index.tsx
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import * as userQueries from 'definitions/user-definitions';
 import * as roleQueries from 'definitions/role-definitions';
 
 import ComboBoxType from './ComboBoxType';
-import Select from 'components/Select';
 
-// graphql
+import Select, { Option } from 'components/Select';
 import withQuery from 'shared/withQuery';
 
-const { Option } = Select;
-const ComboBox = ({ type, textField, valueField, ...others }) => {
+const ComboBox = ({ type, textField, valueField, ...others }, ref) => {
   // defines
   let dataSource = [];
-  let query;
+  let query, options;
   switch (type) {
     case ComboBoxType.User:
       query = userQueries.GET_USERS;
       break;
+
     case ComboBoxType.Role:
       query = roleQueries.GET_ROLES;
       break;
   }
 
   // query
-  const { data, loading } = withQuery(query);
+  const { data, loading } = withQuery(query, options);
   if (loading) return <Select {...others} />;
   switch (type) {
     case ComboBoxType.User:
-      dataSource = data.users;
+      dataSource = data.users.rows;
       break;
     case ComboBoxType.Role:
       dataSource = data.roles;
@@ -36,8 +35,8 @@ const ComboBox = ({ type, textField, valueField, ...others }) => {
 
   // render
   return (
-    <Select {...others}>
-      {dataSource.map(option => (
+    <Select ref={ref} {...others}>
+      {dataSource?.map((option) => (
         <Option key={option[valueField]} value={option[valueField]}>
           {option[textField]}
         </Option>
@@ -45,6 +44,6 @@ const ComboBox = ({ type, textField, valueField, ...others }) => {
     </Select>
   );
 };
-export default ComboBox;
+export default forwardRef(ComboBox);
 
 export { ComboBoxType };
