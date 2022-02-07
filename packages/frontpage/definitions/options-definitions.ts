f0513099f7cd1d204a4858d1f8f82585d339db30
unknown <wooowebsite@gmail.com>
2022-02-07 23:20:29 +0700

    Option: Graphql setup

--- packages/frontpage/definitions/options-definitions.ts
+++ packages/frontpage/definitions/options-definitions.ts
@@ -1,12 +1,14 @@
 import { gql } from '@apollo/client';
 
 export const GET_OPTIONS = gql`
-  query ApiUsers {
-    users @rest(type: "User", path: "/users", method: "GET") {
-      id
-      name
-      avatar
-      age
+  query GetOptions($where: OptionWhere) {
+    options(where: $where) {
+      rows {
+        id
+        name
+        value
+        status
+      }
     }
   }
 `;
